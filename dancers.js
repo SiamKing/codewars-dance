861a2b487da1604d26efc3f356a7e4d7acfe706c
SiamKing <thesiameseelectric@gmail.com>
2017-04-14 13:39:17 -0700

    refactored

--- dancers.js
+++ dancers.js
@@ -1,25 +1,7 @@
 function findChildren(dancingBrigade){
-  let dancers = dancingBrigade.split('').sort((a, b) => {
-    if (a < b) {
-      return -1;
-    } else if (a > b) {
-      return 1;
-    } else {
-      return 0;
-      }
-  })
+  return dancingBrigade
+            .split('')
+            .sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()) || b.localeCompare(a))
+            .join('')
 
-  let moms = dancers.filter(v => v <= 'Z');
-  let kids = dancers.filter(v => v >= 'a');
-  let sortedDancers = '';
-
-  moms.forEach(mom => {
-    sortedDancers += mom;
-    for (let i = 0; i < kids.length; i++) {
-        if (kids[i] === mom.toLowerCase()) {
-          sortedDancers += kids[i];
-        }
-    }
-  })
-  return sortedDancers;
-}
+};
